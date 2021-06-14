285bf3e1dcb47407260a8c2e4223c6acc5257fa1
Mirage <mirage4d@hotmail.com>
2021-06-14 16:57:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -162,16 +162,12 @@ const adBreak = (tmiClient) =>{
 
         while(channelList[random] == channel){
             random = Math.floor(Math.random() * (channelList.length - 1 ));
-            messBase2 = "!sh " + channelList[random] + " : " + adList[channelList[random]];
         }
+        messBase2 = "!sh " + channelList[random] + " : " + adList[channelList[random]];
         
         console.log("ad: " + channelList[random]);
 
-        tmiClient.say(channel,messBase);
-        
-        setTimeout(()=>{
-            tmiClient.say(channel,messBase2)
-        },1000);
+        tmiClient.say(channel,messBase).then(tmiClient.say(channel,messBase2));
 
     });
 
